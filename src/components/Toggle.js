929e5067e25b88ed633be63d0aa4742ac978df97
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-06-06 10:02:57 +0300

    master

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  // declaring new state variable and function
+  let [power,switchPower] = useState(false)
+
+  function switcher(){
+    // switchPower(power = "ON")
+    // // switchPower(power = "OFF")
+    switchPower((power) => !power )
+
+  }
+
+  return (
+    <button style={{background : "red"}} onClick={switcher}>{power ? "ON" : "OFF"}</button>
+  )
 }
 
 export default Toggle;
